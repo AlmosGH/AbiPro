05831d1b34ca34a2887f86d7acbd51ddba4a299a
AlmosGH <62188528+AlmosGH@users.noreply.github.com>
2026-09-23 18:38:56 +0200

    Update +page.svelte

--- src/routes/impressum/+page.svelte
+++ src/routes/impressum/+page.svelte
@@ -1,2 +1,2 @@
 <svelte:head><title>Impressum – AbiPro</title></svelte:head>
-<main><h1>Impressum</h1><p>Vor dem Produktivstart sind hier Name und Anschrift des Diensteanbieters, eine Kontaktmöglichkeit sowie gegebenenfalls Vertretungs- und Registerangaben einzutragen.</p></main>
+<main><h1>Impressum</h1><p>Feltöltés alatt.</p></main>
